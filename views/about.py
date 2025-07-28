6bbc9ca7d82a3d593ec7d0701d0eeb7732d6c668
okwuchukwu onwuegbuna <okwuchukwuonwuegbuna@gmail.com>
2025-07-28 15:24:30 +0100

    Update about.py

--- views/about.py
+++ views/about.py
@@ -36,7 +36,7 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-st.link_button("_go to_ our payment :blue[website]", "https://www.jmhpsoftware.com")
+st.link_button(":red[_GO TO_ OUR PAYMENT _WEBSITE_] ", "https://www.jmhpsoftware.com")
 
 import streamlit as st
 
